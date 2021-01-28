ec74ff25d1e9b4dd269d307d6785ae60534baba5
Xybaks <nikolai.berestevich@gmail.com>
2021-01-28 16:20:18 +0300

    Updates

---
+++ precache-manifest.93ec10176cf03a7d3765ba82c405fd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f0540260193ff84983cbe879b7b36e",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "d678a19908db2afb7db4",
+    "url": "/todoList/static/css/main.94cf425d.chunk.css"
+  },
+  {
+    "revision": "1936bd2e90089257e39d",
+    "url": "/todoList/static/js/2.dc4e8de6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todoList/static/js/2.dc4e8de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d678a19908db2afb7db4",
+    "url": "/todoList/static/js/main.662aaa08.chunk.js"
+  },
+  {
+    "revision": "4ff11d22cdb575f4ca9d",
+    "url": "/todoList/static/js/runtime-main.95b9ec23.js"
+  }
+]);
